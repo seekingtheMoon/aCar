6c6f8e0a27292756277c527681898535748c5ee0
seekingtheMoon <2020251610@qq.com>
2024-03-14 00:29:38 +0800

    新增 mapper 扫描注解

--- service-passenger-user/src/main/java/com/ekko/servicepassengeruser/ServicePassengerUserApplication.java
+++ service-passenger-user/src/main/java/com/ekko/servicepassengeruser/ServicePassengerUserApplication.java
@@ -1,9 +1,11 @@
 package com.ekko.servicepassengeruser;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.ekko.servicepassengeruser.mapper")
 public class ServicePassengerUserApplication {
 
     public static void main(String[] args) {
