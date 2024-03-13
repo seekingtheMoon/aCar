5852aa6471de3637c923e2f23c8557a7da265390
seekingtheMoon <2020251610@qq.com>
2024-03-14 00:31:29 +0800

    新增 PassengerUserMapper 接口

---
+++ service-passenger-user/src/main/java/com/ekko/servicepassengeruser/mapper/PassengerUserMapper.java
@@ -0,0 +1,10 @@
+package com.ekko.servicepassengeruser.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ekko.servicepassengeruser.dto.PassengerUser;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface PassengerUserMapper extends BaseMapper<PassengerUser> {
+}
