4695e474c760fed6cae0f126efb6c1c3a916e57d
seekingtheMoon <2020251610@qq.com>
2024-03-13 22:14:09 +0800

    service-passenger-user 新服务骨架搭建

---
+++ service-passenger-user/src/main/java/com/ekko/servicepassengeruser/ServicePassengerUserApplication.java
@@ -0,0 +1,12 @@
+package com.ekko.servicepassengeruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePassengerUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicePassengerUserApplication.class);
+    }
+}
