76eae45593cd0f03bd17462cd0e6b1ad87581c48
seekingtheMoon <2020251610@qq.com>
2024-03-14 00:32:14 +0800

    根据手机号，查询用户信息

--- service-passenger-user/src/main/java/com/ekko/servicepassengeruser/service/UserService.java
+++ service-passenger-user/src/main/java/com/ekko/servicepassengeruser/service/UserService.java
@@ -1,15 +1,30 @@
 package com.ekko.servicepassengeruser.service;
 
+import com.baomidou.mybatisplus.core.injector.methods.SelectByMap;
 import com.ekko.internalcommon.dto.ResponseResult;
+import com.ekko.servicepassengeruser.dto.PassengerUser;
+import com.ekko.servicepassengeruser.mapper.PassengerUserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private PassengerUserMapper passengerUserMapper;
 
 
     public ResponseResult loginOrRegister(String passengerPhone) {
 
         // 根据手机号查询用户信息
+        Map<String, Object> map = new HashMap<>();
+        map.put("passenger_phone", passengerPhone);
+        List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
+        System.out.println(passengerUsers.isEmpty() ? "无记录" : passengerUsers.get(0).getPassengerPhone());
 
         // 判断用户信息是否存在
 
