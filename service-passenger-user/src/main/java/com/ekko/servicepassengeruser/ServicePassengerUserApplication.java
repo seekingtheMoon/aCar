77cafc2aedadf9c000605fe3a04edfa6283ffff0
seekingtheMoon <2020251610@qq.com>
2024-04-04 20:26:24 +0800

    注册 service-passenger-user

--- service-passenger-user/src/main/java/com/ekko/servicepassengeruser/ServicePassengerUserApplication.java
+++ service-passenger-user/src/main/java/com/ekko/servicepassengeruser/ServicePassengerUserApplication.java
@@ -2,10 +2,13 @@ package com.ekko.servicepassengeruser;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.ekko.servicepassengeruser.mapper")
+@EnableDiscoveryClient
 public class ServicePassengerUserApplication {
 
     public static void main(String[] args) {
